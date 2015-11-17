7aa024254160d951f746b1dd52799126026289d5
Pavlin Poličar <pavlin.g.p@gmail.com>
2015-11-17 19:19:32 +0000

    Error with a11y dependency

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import a11y from 'react-a11y';
+//import a11y from 'react-a11y';
 import routes from './routes';
 import App from './App.react';
 
 // Uncomment this to get information about accessiblity on devices. Toggle at
 // your own risk.
-//a11y(React, { includeSrcNode: true });
+if (process.env.NODE_ENV !== 'production') {
+  let a11y = require('react-a11y');
+  a11y(React, { includeSrcNode: true });
+}
 
 ReactDOM.render(routes, document.getElementById('react-root'));
